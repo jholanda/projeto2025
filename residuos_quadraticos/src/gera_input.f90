40a913c601c961028f7af9dc3a7bd2e90963f8f0
João Holanda <joao.holanda@gmail.com>
2025-05-17 10:09:59 -0300

    ajustes na configuração do ambiente

--- residuos_quadraticos/src/gera_input.f90
+++ residuos_quadraticos/src/gera_input.f90
@@ -1,30 +1,30 @@
 program gera_input
-    implicit none
-   
-    integer :: i, out, a, b, k
-    real :: x, r, fator_aleatorio, noise, polinomio
+   implicit none
 
-    open(unit=out, file="input.txt", status="replace", action="write")
+   integer :: i, out, a, b, k
+   real :: x, r, fator_aleatorio, noise, polinomio
 
-    a = 1
-    b = 2
-    k = 5
-    
-    do i = 1, 100, +2
+   open (unit=out, file="input.txt", status="replace", action="write")
+
+   a = 1
+   b = 2
+   k = 5
+
+   do i = 1, 100, +2
       x = x + 1
       !call random_number(r)
-      !noise = k * r * (a*x + b) 
+      !noise = k * r * (a*x + b)
       call random_number(r)
-      call random_number(fator_aleatorio)  
-      noise = (k * 2.0 * r) * (fator_aleatorio * a * x + b**2)
+      call random_number(fator_aleatorio)
+      noise = (k*2.0*r)*(fator_aleatorio*a*x + b**2)
       polinomio = a*x + b + noise
       !print *, 'r: ', r
       !print *, 'x: ', x
       !print *, 'noise: ', noise
-      !print *, 'ax + b + k.rand: ', polinomio 
-      write(out, "(F8.2,1X,F8.2)") x, polinomio
-    end do
+      !print *, 'ax + b + k.rand: ', polinomio
+      write (out, "(F8.2,1X,F8.2)") x, polinomio
+   end do
 
-    close(out)
+   close (out)
 
 end program gera_input
