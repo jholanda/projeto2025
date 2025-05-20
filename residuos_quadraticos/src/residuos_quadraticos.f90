1384ed5174f5efe9e374285f4b55369bd117b847
João Holanda <joao.holanda@gmail.com>
2025-05-20 18:48:51 -0300

    acerto da chamada do dgels e retirada de código desnecessário

--- residuos_quadraticos/src/residuos_quadraticos.f90
+++ residuos_quadraticos/src/residuos_quadraticos.f90
@@ -1,13 +1,11 @@
 program residuos_quadraticos
-
-   implicit none
+   implicit none(external)
 
    integer :: io, io_status, i, j, n, m, nrhs, lda, ldb, info
    integer, allocatable :: ipiv(:)
    character(len=512) :: io_msg, filename, arg, fmt_string
    logical :: file_exists
    real :: work(50)
-
    real(kind=8), allocatable :: A(:, :), AT(:, :), ATA(:, :), b(:), ATb(:), x(:)
 
    if (command_argument_count() < 2) then
@@ -17,10 +15,9 @@ program residuos_quadraticos
       call get_command_argument(1, filename)
       call get_command_argument(2, arg)
    end if
-
    read (arg, *, iostat=io_status) n
-   inquire (file=trim(filename), exist=file_exists)
 
+   inquire (file=trim(filename), exist=file_exists)
    if (.not. file_exists) then
       print *, 'Arquivo não encontrado:', trim(filename)
       stop
@@ -44,57 +41,36 @@ program residuos_quadraticos
    allocate (ipiv(n))
 
    open (newunit=io, file=trim(filename), status="old", action="read")
-
-   !do i = 1, m
-   !   do j = 1, n
-   !      A(i, j) = 1.0
-   !   end do
-   !   read (io, *) A(i, n), b(i)
-   ! end do
-
    do i = 1, m
-      A(i, 1) = 1.0  ! Primeira coluna sempre 1 para o termo constante
-      read (io, *) A(i, 2), b(i)  ! Lendo o valor de x e de b
+      A(i, 1) = 1.0
+      read (io, *, iostat=io_status, iomsg=io_msg) A(i, 2), b(i)
 
       do j = 3, n
-         A(i, j) = A(i, j - 1)*A(i, 2)  ! Construindo potências do x
+         A(i, j) = A(i, j - 1)*A(i, 2)
       end do
    end do
-
    close (io)
 
-   print *, "Matrix A:", shape(A)
-
    AT = transpose(A)
-
-   print *, "Matrix AT:", shape(AT)
-
    ATA = matmul(AT, A)
-
-   print *, "Matrix ATA:", shape(ATA)
-
    ATb = matmul(AT, b)
 
-   print *, "Matrix ATb:", shape(ATb)
-
-   !call dgels('N', m, n, 1, ATA, m, ATb, m, work, -1, info)
-   call dgesv(n, 1, ATA, n, ipiv, ATb, n, info)
+   call dgels('N', n, n, 1, ATA, n, ATb, n, work, 50, info)
 
    if (info /= 0) then
       print *, "Erro ao resolver o sistema: código", info
       stop
    end if
 
-   x = ATb
+   x = ATb(1:n)
 
    open (unit=20, file=trim(filename)//"_grau"//trim(arg)//".txt", status="replace", action="write")
    write (20, "(15F18.3)") (x(j), j=1, n)
    flush (20)
    close (20)
 
-   !print *, "Resultado salvo em r", filename
-   !write (20, "(15F18.3)") (x(j), j=1, n)
-
    deallocate (A, AT, ATA, b, ATb, x, ipiv)
-end program residuos_quadraticos
 
+   print *, "Coeficientes calculados com sucesso e salvos em: ", trim(filename)//"_grau"//trim(arg)//".txt"
+
+end program residuos_quadraticos
